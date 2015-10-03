b6be328ca0ed54ea2acaec45023f61ade4851860
Jeremy Karlsson <hello@jeremy.se>
2015-10-03 15:50:51 +0200

    Image loading improv

---
+++ static/index.js
@@ -0,0 +1,22 @@
+var teaseImages = document.querySelectorAll('template.tease');
+
+function showImage(tease) {
+  var parent = tease.parentNode;
+  var image = tease.innerHTML;
+  parent.removeChild(tease);
+  parent.innerHTML += image;
+}
+
+window.onload = function() {
+	if ('content' in document.createElement('template')) {
+		for (var i = 8; i < teaseImages.length; i++) {
+      showImage(teaseImages[i]);
+		}
+	}
+};
+
+if ('content' in document.createElement('template')) {
+  for (var i = 0; i < 8; i++) {
+    showImage(teaseImages[i]);
+  }
+}
